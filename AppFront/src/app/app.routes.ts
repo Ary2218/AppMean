15dc3f50eceecbca6d6580068e974c3075977427
Ary <ary.alberto1810@gmail.com>
2026-02-18 19:38:41 -0800

    Ruta para home

--- AppFront/src/app/app.routes.ts
+++ AppFront/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { SongsList } from './componentes/songs/songs-list/songs-list';
 import { DetailSong } from './componentes/songs/detail-song/detail-song';
 import { AddSong } from './componentes/songs/add-song/add-song';
+import { Home } from './componentes/home/home'
 
 export const routes: Routes = [
+    {path: '', component: Home},
     {path: 'songs', component: SongsList},
     {path: 'songs/add', component: AddSong},
     {path: 'songs/:songId', component: DetailSong}
